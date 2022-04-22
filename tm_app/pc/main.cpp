7c84466b2bb0b6f1911d192215cc0f7924544f13
meetpatty <thoriginalmeetpatty@noreply.github.com>
2022-04-22 13:27:37 +1200

    Add support for 1.00 and 1.00 Bogus to installer

--- tm_app/pc/main.cpp
+++ tm_app/pc/main.cpp
@@ -15,6 +15,10 @@
 #include <common.h>
 
 #include "tm_sloader.h"
+#include "100_payload.h"
+#include "100_tmctrl100.h"
+#include "100B_payload.h"
+#include "100B_tmctrl100B.h"
 #include "150_payload.h"
 #include "150_tmctrl150.h"
 #include "200_payload.h"
@@ -30,6 +34,7 @@
 #include "271_vshmain.h"
 
 using namespace std;
+namespace fs = std::filesystem;
 
 void help(const char* exeName) {
     cout << "Usage: " << exeName << " [OPTION]..." << endl;
@@ -37,11 +42,12 @@ void help(const char* exeName) {
     cout << "Creates TM firmware directory for the specified version." << endl;
     cout << endl;
     cout << "General options:" << endl;
-    cout << "  -h, --help         display this help and exit" << endl;
-    cout << "  -v, --verbose      enable verbose mode (mostly for debugging)" << endl;
-    cout << "  -t, --tmdir=DIR    path to TM directory" << endl;
-    cout << "  -u, --updir=DIR    path to updater pbps" << endl;
-    cout << "  -V, --version=VER  TM firmware version. Supported versions are: 1.50, 2.00, 2.50, 2.71SE-C" << endl;
+    cout << "  -h, --help              display this help and exit" << endl;
+    cout << "  -v, --verbose           enable verbose mode (mostly for debugging)" << endl;
+    cout << "  -t, --tmdir=DIR         path to TM directory" << endl;
+    cout << "  -u, --updir=DIR         path to updater pbps" << endl;
+    cout << "  -V, --version=VER       TM firmware version. Supported versions are: 1.00, 1.00Bogus, 1.50, 2.00, 2.50, 2.71SE-C" << endl;
+    cout << "  -d, --downdaterdir=DIR  path to 1.00 downdater dump. Required for 1.00 and 1.00Bogus firmware installs" << endl;
 }
 
 int readFile(string path, char **inData) {
@@ -64,7 +70,26 @@ int readFile(string path, char **inData) {
     return size;
 }
 
-void extract150Ipl(u8 *dataPsp, u32 size, string installDir, bool verbose) {
+void extract100Ipl(string iplPath, string installPath, bool verbose) {
+    
+    string logStr;
+    string tmpDir = installPath + "/tmp";
+    char *buf;
+
+    fs::create_directory(tmpDir); 
+
+    int size = readFile(iplPath, &buf);
+    decryptIPL((u8*)buf, size, 100, "psp_ipl.bin", tmpDir, nullptr, 0, verbose, false, logStr);
+    std::cout << "Decrypting 1.00 ipl" << logStr << std::endl;  
+
+    fs::copy(tmpDir + "/stage1_psp_ipl.bin", installPath + "/nandipl.bin", fs::copy_options::overwrite_existing);
+
+    fs::remove_all(tmpDir);
+
+    delete[] buf;
+}
+
+void extract150Ipl(u8 *dataPsp, u32 size, string installPath, bool verbose) {
     
     string logStr;
     char *outData = new char[0x400000];
@@ -76,8 +101,10 @@ void extract150Ipl(u8 *dataPsp, u32 size, string installDir, bool verbose) {
     // extract ipl from ipl updater module
     cout << "Extracting ipl" << endl;
     outSize = pspDecryptPRX((const u8 *)&outData[0x303100], (u8 *)updaterData, 0x38480, nullptr, true);
-    decryptIPL((u8*)&updaterData[0x980], 0x37000, 150, "psp_ipl.bin", installDir + "/PSARDUMPER", nullptr, 0, verbose, false, logStr);
+    decryptIPL((u8*)&updaterData[0x980], 0x37000, 150, "psp_ipl.bin", installPath + "/PSARDUMPER", nullptr, 0, verbose, false, logStr);
     std::cout << logStr << std::endl;
+
+    fs::copy(installPath + "/PSARDUMPER/stage1_psp_ipl.bin", installPath + "/nandipl.bin", fs::copy_options::overwrite_existing);
 
     delete[] outData;
     delete[] updaterData;
@@ -108,7 +135,7 @@ void updateBtCfg(string path, const char* regex, const char *replacement) {
     delete[] cfgBuf;
 }
 
-void createSystemCtrl(string systemCtrlPath, string rebootBinPath, const unsigned char *systemCtrlBuf, int systemCtrlSize, int rebootBinOffset, bool gzip)
+void createSystemCtrl(string systemCtrlPath, string rebootBinPath, const unsigned char *systemCtrlBuf, int systemCtrlSize, int rebootBinOffset, bool gzip) 
 {
     char *rebootBuf;
     char *outBuf = new char[systemCtrlSize];
@@ -128,8 +155,8 @@ void createSystemCtrl(string systemCtrlPath, string rebootBinPath, const unsigne
     delete[] compressedBuf;
 }
 
-void extractFirmware(string installPath, string upDir, string version, bool deletePsarDumper, bool verbose)
-{
+void extractFirmware(string installPath, string upDir, string version, bool deletePsarDumper, bool verbose) {
+
     char *buf;
     string installerPath = upDir + "/" + version + ".PBP";
 
@@ -144,14 +171,13 @@ void extractFirmware(string installPath, string upDir, string version, bool dele
 
         if (version.compare("150") == 0) {
             extract150Ipl((u8*)&buf[pspOff], psarOff - pspOff, installPath, verbose);
-            std::filesystem::copy(installPath + "/PSARDUMPER/stage1_psp_ipl.bin", installPath + "/nandipl.bin", std::filesystem::copy_options::overwrite_existing);
         }
 
-        if (std::filesystem::exists(installPath + "/PSARDUMPER/stage1_psp_ipl.bin"))
-            std::filesystem::copy(installPath + "/PSARDUMPER/stage1_psp_ipl.bin", installPath + "/nandipl.bin", std::filesystem::copy_options::overwrite_existing);
+        if (fs::exists(installPath + "/PSARDUMPER/stage1_psp_ipl.bin"))
+            fs::copy(installPath + "/PSARDUMPER/stage1_psp_ipl.bin", installPath + "/nandipl.bin", fs::copy_options::overwrite_existing);
 
         if (deletePsarDumper)
-            std::filesystem::remove_all(installPath + "/PSARDUMPER");
+            fs::remove_all(installPath + "/PSARDUMPER");
 
         // tidy up
         delete[] buf;
@@ -172,6 +198,7 @@ int main(int argc, char *argv[]) {
         {"tmdir",        required_argument, 0, 't'},
         {"updir",        required_argument, 0, 'u'},
         {"version",      required_argument, 0, 'V'},
+        {"downdaterdir", required_argument, 0, 'd'},
         {0,              0,                 0,  0 }
     };
     int long_index;
@@ -180,9 +207,10 @@ int main(int argc, char *argv[]) {
     string upDir = ".";
     bool verbose = false;
     string version;
+    string downdaterDir;
     int c = 0;
     cout << showbase << internal << setfill('0');
-    while ((c = getopt_long(argc, argv, "hvo:t:u:V:", long_options, &long_index)) != -1) {
+    while ((c = getopt_long(argc, argv, "hvo:t:u:V:d:", long_options, &long_index)) != -1) {
         switch (c) {
             case 'h':
                 help(argv[0]);
@@ -199,6 +227,9 @@ int main(int argc, char *argv[]) {
             case 'V':
                 version = string(optarg);
                 break;
+            case 'd':
+                downdaterDir = string(optarg);
+                break;
             default:
                 help(argv[0]);
                 return 1;
@@ -209,12 +240,54 @@ int main(int argc, char *argv[]) {
     char* outData;
     int size;
 
-    if (!std::filesystem::exists(tmDir)) {
+    if (!fs::exists(tmDir)) {
         std::cerr << "Specified TM directory does not exist!" << std::endl;
         return 1;
     }
 
-    if (version.compare("1.50") == 0) {
+    if ((version.compare("1.00Bogus") == 0) || (version.compare("1.00Bogus") == 0))
+    {
+        if (downdaterDir.empty()) {
+            std::cerr << "Downdater required for 1.00 firmware installs" << std::endl;
+            return 1;
+        }
+        if (!fs::exists(downdaterDir)) {
+            std::cerr << "Downdater directory not found" << std::endl;
+            return 1;
+        }
+        if (!fs::exists(downdaterDir + "/ipl.bin") || 
+            !fs::exists(downdaterDir + "DUMP")) {
+            std::cerr << "Downdater directory does not contain expected contents" << std::endl;
+            return 1;
+        }
+    }
+
+    if (version.compare("1.00") == 0) {
+        string installDir = tmDir + "/100";
+        
+        const auto copyOptions = fs::copy_options::overwrite_existing 
+                               | fs::copy_options::recursive;
+
+        fs::copy(downdaterDir + "/DUMP/", installDir, copyOptions);
+        fs::create_directory(installDir + "registry");
+
+        extract100Ipl(downdaterDir + "/ipl.bin", installDir, verbose);
+
+        WriteFile((installDir + "/tm_sloader.bin").c_str(), (void*)tm_sloader, sizeof(tm_sloader));
+        WriteFile((installDir + "/payload.bin").c_str(), (void*)tm100_payload, sizeof(tm100_payload));
+        WriteFile((installDir + "/tmctrl100.prx").c_str(), (void*)tm100_tmctrl100, sizeof(tm100_tmctrl100));
+    }
+    if (version.compare("1.00Bogus") == 0) {
+        string installDir = tmDir + "/100_Bogus";
+        extractFirmware(installDir, upDir, "100", true, verbose);
+
+        extract100Ipl(downdaterDir + "/ipl.bin", installDir, verbose);
+
+        WriteFile((installDir + "/tm_sloader.bin").c_str(), (void*)tm_sloader, sizeof(tm_sloader));
+        WriteFile((installDir + "/payload.bin").c_str(), (void*)tm100B_payload, sizeof(tm100B_payload));
+        WriteFile((installDir + "/tmctrl100_Bogus.prx").c_str(), (void*)tm100B_tmctrl100B, sizeof(tm100B_tmctrl100B));
+    }
+    else if (version.compare("1.50") == 0) {
         string installDir = tmDir + "/150";
         extractFirmware(installDir, upDir, "150", true, verbose);
 
@@ -249,17 +322,17 @@ int main(int argc, char *argv[]) {
         updateBtCfg(installDir + "/kd/pspbtcnf_updater.txt", "/kd/vshbridge.prx", "/kd/vshbridge.prx\n/kd/systemctrl.prx");
 
         //Remove unused 1.50 firmware files
-        std::filesystem::remove_all(installDir + "/vsh/etc/");
-        std::filesystem::remove_all(installDir + "/vsh/resource/");
-        std::filesystem::remove(installDir + "/kd/mebooter_umdvideo.prx");
-        std::filesystem::remove(installDir + "/kd/reboot.prx");
+        fs::remove_all(installDir + "/vsh/etc/");
+        fs::remove_all(installDir + "/vsh/resource/");
+        fs::remove(installDir + "/kd/mebooter_umdvideo.prx");
+        fs::remove(installDir + "/kd/reboot.prx");
 
         const char *plugins[11] = { "auth_plugin", "game_plugin", "impose_plugin", "msgdialog_plugin",
                                     "msvideo_plugin", "music_plugin", "opening_plugin", "photo_plugin",
                                     "sysconf_plugin", "update_plugin", "video_plugin" };
 
         for (int i = 0; i < 11; i++) {
-            std::filesystem::remove(installDir + "/vsh/module/" + plugins[i] + ".prx");
+            fs::remove(installDir + "/vsh/module/" + plugins[i] + ".prx");
         }
 
         WriteFile((installDir + "/tm_sloader.bin").c_str(), (void*)tm_sloader, sizeof(tm_sloader));
@@ -272,15 +345,15 @@ int main(int argc, char *argv[]) {
         //Setup 271
         extractFirmware(tmpDir, upDir, "271", false, verbose);
 
-        const auto copyOptions = std::filesystem::copy_options::overwrite_existing 
-                               | std::filesystem::copy_options::recursive;
+        const auto copyOptions = fs::copy_options::overwrite_existing 
+                               | fs::copy_options::recursive;
 
-        std::filesystem::copy(tmpDir + "/data/", installDir + "/data/", copyOptions);
-        std::filesystem::copy(tmpDir + "/font/", installDir + "/font/", copyOptions);
-        std::filesystem::copy(tmpDir + "/kd/", installDir + "/kn/", copyOptions);
-        std::filesystem::copy(tmpDir + "/vsh/etc/", installDir + "/vsh/etc/", copyOptions);
-        std::filesystem::copy(tmpDir + "/vsh/module/", installDir + "/vsh/nodule/", copyOptions);
-        std::filesystem::copy(tmpDir + "/vsh/resource/", installDir + "/vsh/resource/", copyOptions);
+        fs::copy(tmpDir + "/data/", installDir + "/data/", copyOptions);
+        fs::copy(tmpDir + "/font/", installDir + "/font/", copyOptions);
+        fs::copy(tmpDir + "/kd/", installDir + "/kn/", copyOptions);
+        fs::copy(tmpDir + "/vsh/etc/", installDir + "/vsh/etc/", copyOptions);
+        fs::copy(tmpDir + "/vsh/module/", installDir + "/vsh/nodule/", copyOptions);
+        fs::copy(tmpDir + "/vsh/resource/", installDir + "/vsh/resource/", copyOptions);
 
         //Decrypt boot config files and insert systemctrl.prx
         updateBtCfg(installDir + "/kn/pspbtcnf.txt", "/kd/modulemgr.prx", "/kd/modulemgr.prx\n/kd/systemctrl.prx");
@@ -293,8 +366,8 @@ int main(int argc, char *argv[]) {
         createSystemCtrl(installDir + "/kn/systemctrl.prx", installDir + "/PSARDUMPER/reboot.bin", 
                             tm271_systemctrl, sizeof(tm271_systemctrl), 0xa27c, true);
 
-        std::filesystem::remove_all(tmpDir);
-        std::filesystem::remove_all(installDir + "/PSARDUMPER");        
+        fs::remove_all(tmpDir);
+        fs::remove_all(installDir + "/PSARDUMPER");        
     }
     else {
         cerr << "Unsupported version " << version << "!" << endl;
