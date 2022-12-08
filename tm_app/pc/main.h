3f1d29f646e3b94970c7e1f550b7cffeee329de1
meetpatty <thoriginalmeetpatty@noreply.github.com>
2022-12-08 21:05:11 +1300

    Simplify tm pc app cmake file.

---
+++ tm_app/pc/main.h
@@ -0,0 +1,56 @@
+#pragma once
+
+// Loaders
+#include "tm_sloader.h"
+#include "tm_mloader.h"
+
+// 1.00
+#include "tm100_payload.h"
+#include "tm100_tmctrl100.h"
+
+// 1.00 Bogus
+#include "tm100B_payload.h"
+#include "tm100B_tmctrl100B.h"
+#include "tm100B_vshex.h"
+
+// 1.50
+#include "tm150_payload.h"
+#include "tm150_tmctrl150.h"
+
+// 2.00
+#include "tm200_payload.h"
+#include "tm200_tmctrl200.h"
+
+// 2.50
+#include "tm250_payload.h"
+#include "tm250_tmctrl250.h"
+
+// 2.60
+#include "tm260_payload.h"
+#include "tm260_tmctrl260.h"
+
+// 2.71 SE-C
+#include "tm271_payload.h"
+#include "tm271_tmctrl150.h"
+#include "tm271_tmctrl271.h"
+#include "tm271_systemctrl150.h"
+#include "tm271_systemctrl.h"
+#include "tm271_paf.h"
+#include "tm271_vshmain.h"
+
+// 2.80
+#include "tm280_payload.h"
+#include "tm280_tmctrl280.h"
+
+// 3.40 OE-A
+#include "tm340_payload.h"
+#include "tm340_tmctrl150.h"
+#include "tm340_tmctrl340.h"
+#include "tm340_idcanager.h"
+#include "tm340_lcpatcher.h"
+#include "tm340_popcorn.h"
+#include "tm340_reboot150.h"
+#include "tm340_recovery.h"
+#include "tm340_systemctrl.h"
+#include "tm340_systemctrl150.h"
+#include "tm340_vshctrl.h"
